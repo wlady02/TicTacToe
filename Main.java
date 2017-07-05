80148aa089c1bd7bbf918df9dc3e10d027330aca
wlady02 <rimid22021991@gmail.com>
2017-07-05 12:56:21 +0200

    conexion socket

---
+++ Main.java
@@ -0,0 +1,11 @@
+package ClientTicTacToe;
+//import javax.swing.JFrame;
+public class Main {
+	public static void main(String[] args) {
+		Game play = new Game();
+		play.setBoard();
+		//play.setBoard();
+		//play.getMove();
+		//play.setMove();
+	}
+}
